4f3c6f39dbadac0a2abf1d60f1d10767b1007eb0
alessandroscoelho <alessandroscoelho@gmail.com>
2025-06-07 10:08:13 -0300

    teste

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -7,7 +7,7 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
-    const currency = 'R$';
+    const currency = 'R$ ';
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
